3f60a61a9f8e726bd524d665d52ee694ee39abf2
tushar0910 <74850063+tushar0910@users.noreply.github.com>
2025-03-25 01:46:36 +0530

    main.py

--- main.py
+++ main.py
@@ -1,27 +1,57 @@
-from flask import Flask, request, jsonify
-import joblib
+from flask import Flask, request, Response
+import functools
+import pickle
+import json
+import os
 
-# Initialize Flask app
-app = Flask(__name__)
+from sklearn.feature_extraction.text import CountVectorizer
 
-# Load model and vectorizer
-model = joblib.load("news_category_classifier")
-vectorizer = joblib.load("vectorizer.pkl")
+app = Flask( __name__ )
 
-# Define /classify endpoint
-@app.route("/classify", methods=["POST"])
-def classify():
-    data = request.get_json()
-    if not data or "headline" not in data:
-        return jsonify({"error": "Missing 'headline' in request"}), 400
 
-    headline = data["headline"]
-    X = vectorizer.transform([headline])
-    prediction = model.predict(X)[0]
+@app.route( "/classify", methods = [ "POST" ] ) # when I post to the {base}/classify URL I call this function
+def classify_article( ):
+    """
+    Classifies a news article given its "headline" and "shortDescription".
+    """
 
-    return jsonify({"category": prediction})
+    #   Load the classifier
+    classifier = _load_classifier( )
 
-# Optional: Root route for health check
-@app.route("/", methods=["GET"])
-def home():
-    return jsonify({"message": "News Classifier API is running"}), 200
+    #   Classify the input normally
+    request_json = request.get_json( )
+    headline = request_json.get( "headline", None )
+    if headline is not None:
+
+        #   Make the prediction
+        content = f"{headline}"
+        with open('vectorizer.pkl', 'rb') as file:
+            loaded_vectorizer = pickle.load(file)
+        content = loaded_vectorizer.transform([content]).toarray()
+        prediction = classifier.predict( content )[0]
+        print(prediction)
+
+        #   Build the response
+        response_json = { "predictedClass": prediction }
+        response_json_string = json.dumps( response_json )
+        response = Response( response_json_string,  mimetype='application/json' )
+        return response
+
+    #   We are missing content, complain to the user
+    else:
+        response_json = { "message": "headline are required to make a class prediction" }
+        response_json_string = json.dumps( response_json )
+        response = Response( response_json_string,  mimetype='application/json' )
+        return response
+
+
+@functools.lru_cache( maxsize = 1 )
+def _load_classifier():
+    """ Loads the classifier from the local file system. """
+    with open('model.pkl', 'rb') as file:
+        classifier = pickle.load(file)
+    return classifier
+
+
+if __name__ == "__main__":
+    app.run( debug = True, host = "0.0.0.0", port = int( os.environ.get( "PORT", 5000) ) )
